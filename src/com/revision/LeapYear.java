30537ab0351979d656c06ed909c8e9109410acb6
Amol Ghotale <ghotaleamol@gmail.com>
2022-07-03 12:12:23 +0530

    ADDED: Leap year with better approach

---
+++ src/com/revision/LeapYear.java
@@ -0,0 +1,22 @@
+package com.revision;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void leapYear() {
+        System.out.println("Enter the year: ");
+        int year = scan.nextInt();
+        if (year % 4 == 0) {
+            System.out.println(year + " is a leap year");
+        } else
+            System.out.println(year + " is not a leap year");
+    }
+
+    public static void main(String[] args) {
+        leapYear();
+   
+
+    }
+}
